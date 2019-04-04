b6d62a654eff9b084dac500a51ff2a01ba28ae49
WildPastry <mykdsn@gmail.com>
2019-04-04 15:28:22 +1300

    Method Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./scss/main.scss";
+
+// IMPORT AXIOS
+// import axios from "axios";
+// IMPORT LOCAL DATA
+
+import behanceDataFROMJSON from "./data/behanceData";
+console.log(" ");
+console.log("data loaded...");
+console.log(behanceDataFROMJSON);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid">
       </div>
     );
   }
