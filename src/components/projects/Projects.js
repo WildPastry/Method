c54a1b930248cf781077b91e5ee32d8f7588d365
mariawenrose <mariarose.wen@gmail.com>
2019-04-09 10:03:33 +1200

    map within map up and running

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -19,38 +19,87 @@ class Projects extends Component {
 
   render() {
     var projectDetails = this.props.projectsState.behanceData;
-    console.log(projectDetails); 
-    console.log("...");
-    console.log(projectDetails[0].projects[0].name);
-    console.log('...hello');
-    console.log(projectDetails[0].projects[0].covers[202]);
-    console.log("...");
-    console.log(projectDetails[0].projects[0].stats.views);
+    // here it's defined
+
+    console.log(projectDetails);
+    for (let index = 0; index < projectDetails.length; index++) {
+      const element = projectDetails[index];
+      console.log(element['projects'])
+      
+    }
+    // console.log("...");
+    // console.log(projectDetails[0].projects[0].name);
+    // console.log('...');
+    // console.log(projectDetails[0].projects[0].covers[202]);
+    // console.log("...");
+    // console.log(projectDetails[0].projects[0].stats.views);
 
 
 
+// return (
+//   projectDetails.map((projectDetailsMapped => (
+//        <div className="col-3">
+//         <p className = "textLight text-center">{projectDetails[0].projects[0].name}</p>
+//         <p className = "textLight text-center">{projectDetails[0].projects[0].stats.views}</p>
+//         <img src = {projectDetails[0].projects[0].covers[230]}/>
+//        </div>
+//         )
+//      ))
+//     ) 
+//   }
+// }
 
-    // let projects = this.state.projects;
-    // console.log(this.state.projects[0]);
-    // console.log(this.state.projects[0].name[0]);
-    // console.log("Projects Component");
 
-    return (
-      projectDetails.map((projectDetailsMapped => (
-           <div className="col-3">
-           <p className = "textLight text-center">{projectDetails[0].projects[0].name}</p>
-           <p className = "textLight text-center">{projectDetails[0].projects[0].stats.views}</p>
-
-           <img src = {projectDetails[0].projects[0].covers[202]}/>
-
-    </div>
+return (
+  projectDetails.map((projectDetailsMapped => (
+       <div
+        key={projectDetailsMapped}
+        className="col-3"
+       >
+        {
+          projectDetailsMapped['projects'].map((single, i) => {
+            return <div>
+                <p>{single.name}</p>
+              </div>
+          })
+        }
+       </div>
         )
-      ))
-    ) 
+     )) 
+    )
   }
 }
 
+
+
+
+
+// return (
+//   projectDetails.projects.map(projectDetailsMapped => (
+    
+//     <div
+//     key = {projectDetailsMapped.projects.id}
+//     className="col-3">
+//     <p className = "textLight text-center">{projectDetailsMapped.projects.name}</p>
+//     </div>
+//   ))
+
+
+
+
 export default Projects;
+
+
+
+
+
+
+
+
+
+
+
+
 
 {/* <div className="row">
 <h5 className="textLight text-center">Projects</h5>
