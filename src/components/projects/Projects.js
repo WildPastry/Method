f0d675d80d2750c48628e29ea839cdc2261268a6
mariawenrose <mariarose.wen@gmail.com>
2019-04-09 14:27:36 +1200

    95% complete

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -21,26 +21,28 @@ class Projects extends Component {
     var projectDetails = this.props.projectsState.behanceData;
     // here it's defined
 
-    console.log(projectDetails);
-    for (let index = 0; index < projectDetails.length; index++) {
-      const element = projectDetails[index];
-      console.log(element['projects'])
+    // console.log(projectDetails);
+    // for (let index = 0; index < projectDetails.length; index++) {
+    //   const element = projectDetails[index];
+    //   console.log(element['projects'])
       
-    }
+    // }
 
 return (
   projectDetails.map((projectDetailsMapped => (
        <div
-        key={projectDetailsMapped}
-        className=" col-xs-12 col-sm-6 col-md-4 col-lg-4"
+        key={projectDetailsMapped.id}
+        className="col-xs-12 col-sm-6 col-md-4 col-lg-4"
        >
         {
           projectDetailsMapped['projects'].map((singleProject, i) => {
-            return <div className = "wrapperCol cardProject cardWrapper bgLightPink textDark">
+            return <div className = "cardProject cardWrapper bgLightPink textDark"
+            // key={['projects']}
+            >
                 <div className = "cardProject--Img">
-                
                 <img className = "designerProject--Img" src = {singleProject.covers['max_808']}/>
                 </div>
+                
                 <div className = "wrapperProject--Details">
                   <div className = "wrapper paraStyle--DesignerProject">
                     <p className = "textDark">{singleProject.name}</p>
