e274ad12374ddb27aa074038b53d7a17ae89d1ab
mariawenrose <mariarose.wen@gmail.com>
2019-04-11 15:23:26 +1200

    starting search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,15 @@ import React, { Component } from "react";
 // USER DATA
 class Search extends Component {
   render() {
-    // console.log("Search Component");
+
+    var projectSearch = this.props.searchState.behanceData;
+    console.log(projectSearch);
     return (
-        <div className={this.props.searchState.cardClass}>
-          <h5 className={this.props.searchState.hClass}>Search</h5>
+        <div id="methodSearch">
+        <div className = "methodSearchContainer">
+          <h5 className={this.props.searchState.hClass}>Hello</h5>
+          <input placeholder="search"></input>
+        </div>
         </div>
     );
   }
