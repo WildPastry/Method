fe2261793f1458f02464f88f934ccf5aecb97130
Daniel edward robert Clarke <932400925@bun2-206-09.local>
2019-04-12 15:00:07 +1200

    modal visually complete, need to create media query for phone size

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -48,33 +48,36 @@ class Modal extends Component {
 
 		return (
 			<div id="methodLoader">
+				<div className="closeModal textLightPink textBold">
+					<h1>X</h1>
+				</div>
 				<div className="methodModal bgDark">
 					<div className="methodModal__top">
 						<img className="methodModal__top--image" src={this.props.modalState.behanceData[projectDeets].projects[0].covers[808]} alt="loading...." />
 					</div>
-					<div className="row wrapperCol bgLightPink textDark methodModal__bottom">
-						<div className="col-6 methodModal__bottomLeft">
-							<div className="methodModal__bottomLeft--Designer">
-								<h4 className="textBold">{this.props.modalState.behanceData[projectDeets].projects[0].name}</h4>
-							</div>
-							<div className="methodModal__bottomLeft--likes">
-								<img src={require("../icons/menu/likesGrey.svg")} alt="loading..." />
-								<h5 className="">Likes: {this.props.modalState.behanceData[projectDeets].projects[0].stats.appreciations}</h5>
-							</div>
-							<div className="methodModal__bottomLeft--views">
-								<img src="../../icons/menu/likesGreen.svg" alt="loading..." />
-								<h5 className="">Views: {this.props.modalState.behanceData[projectDeets].projects[0].stats.views}</h5>
-							</div>
-							<div className="methodModal__bottomLeft--comments">
-								<img src="../../icons/menu/likesGreen.svg" alt="loading..." />
-								<h5 className="">Comments: {this.props.modalState.behanceData[projectDeets].projects[0].stats.comments}</h5>
-							</div>
+					<div className="row bgLightPink textDark methodModal__bottom">
+						<div className="col-7 wrapperCol methodModal__bannerLeft">
+							<h4 className="textBold">{this.props.modalState.behanceData[projectDeets].projects[0].name}</h4>
 						</div>
-						<div className="col-6 methodModal__bottomRight">
-							<div>
-								<h4 className="textBold">Designer Name</h4>
-							</div>
+						<div className="col-5 wrapperCol methodModal__bannerRight">
+							<h4 className="textBold">{this.props.modalState.behanceData[projectDeets].user.username}</h4>
 						</div>
+						<div className="col-4 methodModal__bottomLeft">
+							<img className="methodModal__icons" src={require("../icons/menu/viewsGrey.svg")} alt="loading..." />
+							<h5 className="textThin">Views: {this.props.modalState.behanceData[projectDeets].projects[0].stats.views}</h5>
+						</div>
+						<div className="col-4 methodModal__bottomMiddle">
+							<img className="methodModal__icons" src={require("../icons/menu/likesGrey.svg")} alt="loading..." />
+							<h5 className="textThin">Likes: {this.props.modalState.behanceData[projectDeets].projects[0].stats.appreciations}</h5>
+						</div>
+						<div className="col-4 methodModal__bottomRight">
+							<img className="methodModal__icons" src={require("../icons/menu/commentsGrey.svg")} alt="loading..." />
+							<h5 className="textThin">Comments: {this.props.modalState.behanceData[projectDeets].projects[0].stats.comments}</h5>
+						</div>
+						<div className="col-12 methodModal__bannerBottom">
+							<p className="textThin">{this.props.modalState.behanceData[projectDeets].projects[0].fields[0]} | {this.props.modalState.behanceData[projectDeets].projects[0].fields[1]} | {this.props.modalState.behanceData[projectDeets].projects[0].fields[2]}</p>
+						</div>
+
 
 					</div>
 				</div>
