c86153b80b1eb7660437634461180b024545d99d
Daniel edward robert Clarke <932400925@bun2-206-09.local>
2019-04-12 15:29:00 +1200

    added an svg to the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -63,11 +63,12 @@ class Modal extends Component {
 							<h4 className="textBold">{this.props.modalState.behanceData[projectDeets].user.username}</h4>
 						</div>
 						<div className="col-4 methodModal__bottomLeft">
-							<img className="methodModal__icons" src={require("../icons/menu/viewsGrey.svg")} alt="loading..." />
+							<svg className="methodModal__icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.56 70.38"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path className="methodModal__icons--grey" d="M75.54,33C64.91,21.24,53.66,15.55,42.12,16.1c-18,.84-29.91,16.47-30.41,17.13l-.94,1.27.94,1.27c.53.72,13.28,17.73,31.68,18.16h.72c11.1,0,21.69-6.11,31.51-18.19l1.15-1.41Zm-34.72-3.5a4,4,0,1,1-4-4A4,4,0,0,1,40.82,29.49Zm-24.66,5c2.69-3.09,10.4-10.94,20.94-13.42a15.29,15.29,0,0,0-.84,27.4C26.27,45.5,18.8,37.62,16.16,34.52ZM50.23,48.93a15.29,15.29,0,0,0,.3-27.72c7,1.78,13.88,6.2,20.61,13.28C64.42,42.36,57.42,47.18,50.23,48.93Z"/><path className="methodModal__icons--grey" d="M65.51,0H22.06L0,35.18,22.06,70.37H65.5l22.06-35.2Zm-3,64.59H25L6.57,35.18,25,5.77H62.55L81,35.18Z"/></g></g></svg>
+							{/* <img className="methodModal__icons" src={require("../icons/menu/viewsGrey.svg")} alt="loading..." /> */}
 							<h5 className="textThin">Views: {this.props.modalState.behanceData[projectDeets].projects[0].stats.views}</h5>
 						</div>
 						<div className="col-4 methodModal__bottomMiddle">
-							<img className="methodModal__icons" src={require("../icons/menu/likesGrey.svg")} alt="loading..." />
+							{/* <img className="methodModal__icons" src={require("../icons/menu/likesGrey.svg")} alt="loading..." /> */}
 							<h5 className="textThin">Likes: {this.props.modalState.behanceData[projectDeets].projects[0].stats.appreciations}</h5>
 						</div>
 						<div className="col-4 methodModal__bottomRight">
